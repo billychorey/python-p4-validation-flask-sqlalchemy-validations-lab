4a7ab4b05ff281d7ece1fa7244d666dbe5b4c621
Billy <billychorey@gmail.com>
2024-08-12 17:56:22 -0400

    sqalchemy validation

--- server/models.py
+++ server/models.py
@@ -11,7 +11,14 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators 
+
+    @validates('name', 'phone_number')
+    def validate_author(self, key, value):
+        if key == 'name' and not value:
+            raise ValueError("Name cannot be null")
+        if key == 'phone_number' and len(value) != 10:
+            raise ValueError("Phone number must be exactly 10 digits")
+        return value
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -27,8 +34,17 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
-
+    @validates('content', 'summary', 'category', 'title')
+    def validate_post(self, key, value):
+        if key == 'content' and len(value) < 250:
+            raise ValueError("Content must be at least 250 characters long")
+        if key == 'summary' and len(value) > 250:
+            raise ValueError("Summary must be a maximum of 250 characters")
+        if key == 'category' and value not in ["Fiction", "Non-Fiction"]:
+            raise ValueError("Category must be either 'Fiction' or 'Non-Fiction'")
+        if key == 'title' and not any(phrase in value for phrase in ["Won't Believe", "Secret", "Top", "Guess"]):
+            raise ValueError("Title must contain one of the following: 'Won't Believe', 'Secret', 'Top', 'Guess'")
+        return value
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
